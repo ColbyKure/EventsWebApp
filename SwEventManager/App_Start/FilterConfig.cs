8bcdcee7478d11cb5d53b249bed4f1c427691261
Arsenio Benitez <Arseniobenitezjr@hotmail.com>
2020-06-18 15:30:04 -0400

    Add project files.

---
+++ SwEventManager/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SwEventManager
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SwEventManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SwEventManager.MvcApplication" Language="C#" %>
