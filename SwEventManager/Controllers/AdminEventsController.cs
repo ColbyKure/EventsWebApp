5b2894f2896e1fbf8cd9aaf45adcb08c07d9dfb1
Colby Kure <colbykure@gmail.com>
2020-06-20 21:30:53 -0700

    added Image Upload

--- SwEventManager/Controllers/AdminEventsController.cs
+++ SwEventManager/Controllers/AdminEventsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using SwEventManager.Models;
+using System.IO;
 
 namespace SwEventManager.Controllers
 {
@@ -18,6 +19,30 @@ namespace SwEventManager.Controllers
         public ActionResult Index()
         {
             return View(db.Events.ToList());
+        }
+
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/images/profile"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+
+            }
+            // after successfully uploading redirect the user
+            return RedirectToAction("actionname", "controller name");
         }
 
         // GET: Events/Details/5
