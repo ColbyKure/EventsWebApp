7ed56f9c053c733786d30aa5f02f78c65f8868fe
Colby Kure <colbykure@gmail.com>
2020-06-23 08:18:08 -0700

    unintended change

--- SwEventManager/Models/Event.cs
+++ SwEventManager/Models/Event.cs
@@ -11,7 +11,8 @@ namespace SwEventManager.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.Web.Mvc;
+
     public partial class Event
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
